c387cd92f370d7e352a0f04fa3399ddddaaeb716
inventivetalent <inventivetalent@inventivegames.de>
2017-08-05 22:49:12 +0200

    fix SpiGetOrg/Spiget#10

--- public/documentation/docs/index.php
+++ public/documentation/docs/index.php
@@ -1,3 +1,7 @@
 <?php
-
-echo file_get_contents("https://raw.githubusercontent.com/SpiGetOrg/Documentation/master/swagger.json");
+echo file_get_contents("https://raw.githubusercontent.com/SpiGetOrg/Documentation/master/swagger.json",false,stream_context_create(array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+)));
